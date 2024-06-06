069fc3c125fc9efbc240a780b70918e48de9c1a5
TY_Lee <118336957+Wxm-233@users.noreply.github.com>
2024-06-07 01:15:58 +0800

    clear

--- dist/assets/slidev/default-ClKiAJfY.js
+++
@@ -1,1 +0,0 @@
-import{o,b as t,f as s}from"../modules/vue-DrJwJ16H.js";import{_ as l}from"../index-D18q6Lby.js";const c={},n={class:"slidev-layout default"};function a(e,r){return o(),t("div",n,[s(e.$slots,"default")])}const f=l(c,[["render",a],["__file","D:/Documents/slidev/node_modules/@slidev/client/layouts/default.vue"]]);export{f as I};
